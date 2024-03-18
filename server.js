f3fbf20dfb32ebe0ee294f19964f8d0e6418095f
KayleighPerera <5PEREK11@solent.ac.uk>
2024-03-18 21:56:54 +0000

    update

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get("/form", (req, res) => {
   res.sendFile(path.join(__dirname, "../form.html"));
 });
 
+// Define a route for serving your activities page file
+app.get("/activities", (req, res) => {
+  res.sendFile(path.join(__dirname, "../activites.html"));
+});
+
+// Define a route for serving your form page file
+app.get("/modules", (req, res) => {
+  res.sendFile(path.join(__dirname, "../modules.html"));
+});
+
 // Start the server on port 3000
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
